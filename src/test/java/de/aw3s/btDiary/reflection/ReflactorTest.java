3a9da1e309b0e16d982a82236d3d64e6b4a426f0
iface06 <tobias.tatsch@t-online.de>
2013-11-10 13:13:17 +0100

    .idea and target directory deleted from Repository. maven configuration - pmd (Code Quality) - cobertura (Test Coverage)
    dashboard removed

---
+++ src/test/java/de/aw3s/btDiary/reflection/ReflactorTest.java
@@ -0,0 +1,39 @@
+package de.aw3s.btDiary.reflection;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ReflactorTest {
+
+
+    @Test
+    public void testGetStringProperty(){
+
+        Dummy d = new Dummy();
+        d.setName("hallo");
+
+        String property = new Reflactor().getProperty(d, "name");
+        assertEquals("hallo", property);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testExceptionHandling(){
+        Dummy d = new Dummy();
+
+        String property = new Reflactor().getProperty(d, "noSuchMethode");
+        fail();
+    }
+
+    public static class Dummy{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
